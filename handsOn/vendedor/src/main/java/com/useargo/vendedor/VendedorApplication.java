c5a60a03e3ae66e58af15026280b20eddbd3812b
Igor Floor <igormfloor@gmail.com>
2020-08-03 01:39:28 -0300

    Adding initial project

---
+++ handsOn/vendedor/src/main/java/com/useargo/vendedor/VendedorApplication.java
@@ -0,0 +1,13 @@
+package com.useargo.vendedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendedorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendedorApplication.class, args);
+    }
+
+}
---
+++ handsOn/vendedor/src/test/java/com/useargo/vendedor/VendedorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.useargo.vendedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendedorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
