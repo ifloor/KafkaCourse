3efe494bf867c4bf696cdd79c50508ebd3ff336f
Igor Floor <igormfloor@gmail.com>
2020-08-04 23:56:39 -0300

    Adding initial emissor project

---
+++ handsOn/emissor/src/main/java/br/com/monitoratec/emissor/EmissorApplication.java
@@ -0,0 +1,13 @@
+package br.com.monitoratec.emissor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmissorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmissorApplication.class, args);
+    }
+
+}
